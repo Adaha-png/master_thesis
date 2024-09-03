56eaf9b2d2d094aa8e5dc3b3038b62bb948ba197
Adaha-png <ada.hatland@gmail.com>
2024-09-03 15:05:38 +0200

    save and resume study

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import glob
 import os
 import time
-
+import pickle
 import optuna
 import supersuit as ss
 from pettingzoo.butterfly import knights_archers_zombies_v10
@@ -141,13 +141,14 @@ def tuner(trial):
     )
     la = trial.suggest_float("la", 0.9, 0.99)
     gamma = trial.suggest_float("gamma", 0.8, 0.999)
-    lr = trial.suggest_float("lr", 0.0001, 1)
+    lr = trial.suggest_float("lr", 0.0001, 1, log=True)
 
-    train(env_fn, steps=2_000_000, seed=0, lr=lr, gamma=gamma, la=la, **env_kwargs)
+    train(env_fn, steps=500_000, seed=0, lr=lr, gamma=gamma, la=la, **env_kwargs)
 
     return eval(env_fn, num_games=10, render_mode=None, **env_kwargs)
 
 
 if __name__ == "__main__":
-    study = optuna.create_study(direction="maximize")
-    study.optimize(tuner, n_trials=30)
+        
+    study = optuna.create_study(study_name = "tuning", direction="maximize", load_if_exists=True, storage="")
+    study.optimize(tuner, n_trials=30, n_jobs=5)
