2892a7f6166139d170b7b7036d59f9d1551ebc8f
adaha-png <ada.hatland@gmail.com>
2024-10-04 15:39:49 +0200

    counterfactual progress

---
+++ counterfactuals.py
@@ -0,0 +1,127 @@
+import argparse
+import glob
+import os
+from functools import partial
+from typing import Dict, List
+
+import numpy as np
+import supersuit as ss
+from nsga2.evolution import Evolution
+from nsga2.problem import Problem
+from pettingzoo.butterfly import knights_archers_zombies_v10
+from pettingzoo.mpe import simple_spread_v3
+from stable_baselines3 import PPO
+from stable_baselines3.ppo import CnnPolicy, MlpPolicy
+
+from sim_steps import sim_steps
+
+
+def action_difference(sequence1, actions_list2):
+    # Extract values associated with the key "action" from both lists
+    actions_list1 = [val["action"] for val in sequence1]
+
+    # Initialize a counter for differing values
+    count_differences = 0
+
+    # Iterate over the lengths of both lists, assuming they are the same length
+    for i in range(len(actions_list1)):
+        for j in range(len(actions_list1[i])):
+            if actions_list1[i][j] != np.floor(actions_list2[i][j]):
+                count_differences -= 1
+    return count_differences
+
+
+def reward_difference(env, sequence1, chosen_actions):
+    # Extract values associated with the key "action" from both lists
+    rewards_list1 = [val["reward"] for val in sequence1]
+    chosen_actions = np.floor(chosen_actions)
+
+    sequence2 = sim_steps(
+        env,
+        None,
+        chosen_actions=chosen_actions,
+        num_steps=len(chosen_actions),
+    )
+
+    rewards_list2 = [val["reward"] for val in sequence2]
+
+    diff = 0
+
+    for i in range(len(rewards_list1)):
+        for j in range(len(rewards_list1[i])):
+            diff += rewards_list1[i][j] - rewards_list2[i][j]
+    return diff
+
+
+def counterfactuals(env, sequence: List[Dict]):
+    action_objective = partial(action_difference, sequence1=sequence)
+    reward_objective = partial(reward_difference, sequence1=sequence)
+    Problem(
+        [action_objective, reward_objective],
+        len(sequence) * len(sequence[0]["action"]),
+        [(0, 6)],
+        same_range=True,
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Simulation")
+    parser.add_argument(
+        "-e",
+        "--env",
+        type=str,
+        help="Which environment to use",
+        default="spread",
+    )
+
+    parser.add_argument(
+        "-s",
+        "--steps",
+        type=int,
+        help="Steps to simulate",
+        default=10,
+    )
+
+    args = parser.parse_args()
+    if args.env == "spread":
+        env_fn = simple_spread_v3
+
+        env_kwargs = dict(
+            N=3,
+            local_ratio=0.5,
+            max_cycles=25,
+            continuous_actions=False,
+        )
+    elif args.env == "kaz":
+        env_fn = knights_archers_zombies_v10
+
+        env_kwargs = dict(
+            spawn_rate=6,
+            num_archers=2,
+            num_knights=2,
+            max_zombies=10,
+            max_arrows=10,
+            max_cycles=900,
+            vector_state=True,
+        )
+    else:
+        print("Invalid env entered")
+        exit(0)
+
+    env = env_fn.parallel_env(**env_kwargs)
+
+    try:
+        latest_policy = max(
+            glob.glob(f"{str(env.metadata['name'])}/*.zip"),
+            key=os.path.getctime,
+        )
+        print(latest_policy)
+    except ValueError:
+        print("Policy not found.")
+        exit(0)
+
+    env = ss.pettingzoo_env_to_vec_env_v1(env)
+    env = ss.concat_vec_envs_v1(env, 1, num_cpus=1, base_class="stable_baselines3")
+
+    seq = sim_steps(env, latest_policy, num_steps=10)
+    counterfactuals(env, seq)
--- progress.md
+++ progress.md
@@ -1,12 +1,13 @@
 
 # Reading list
-## Article name
+## Acter
 ### Relevancy
-* bullet
-* points
+* Counterfactual sequences
+* smallest perturbation to avoid outcome
 
 ### Difference
-* more bullet points
+* To avoid reward instead of penalty
+* Multi agent
 
 # Worklog
 ## Week 39
@@ -29,6 +30,16 @@ Cons:
 Argaparse to set environment, timesteps and tuning paramenters from terminal
 
 
+## Week 40
+### Fixed eval function
+* one agent acted randomly
+* evaluated as aec instead of parallel
+
+### Counterfactual sequences
+* action diff objective done
+* reward objective almost done
+* Problem object made
 
 # TODO
-
+* make sim\_steps accept seed, slight changes to ss need to be made.
+* make sim\_steps accept initial observation (might need to make an env wrapper, will probably be a lot of work)
