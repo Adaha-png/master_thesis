7f46adc7a620425d1a37d4e50c161173c9488019
adaha-png <ada.hatland@gmail.com>
2024-10-04 15:39:49 +0200

    counterfactual progress

--- sim_steps.py
+++ sim_steps.py
@@ -13,21 +13,25 @@ from stable_baselines3.ppo import CnnPolicy, MlpPolicy
 def sim_steps(
     env,
     policy,
-    num_steps=10,
+    chosen_actions=None,
+    num_steps=20,
     seed=54,
 ):
-    model = PPO.load(policy)
+    if chosen_actions is None:
+        model = PPO.load(policy)
 
-    env = ss.pettingzoo_env_to_vec_env_v1(env)
-    env = ss.concat_vec_envs_v1(env, 1, num_cpus=1, base_class="stable_baselines3")
-
-    obs = env.reset()
+    obs = env.reset(seed=seed)
     rollout = []
     for step in range(num_steps):
         step_dict = {
             "observation": obs,
         }
-        act = model.predict(obs, deterministic=True)[0]
+
+        if chosen_actions is None:
+            act = model.predict(obs, deterministic=True)[0]
+        else:
+            act = chosen_actions[step]
+
         obs, reward, termination, info = env.step(act)
 
         step_dict.update(
@@ -49,9 +53,26 @@ def sim_steps(
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Simulation")
     parser.add_argument(
-        "-e", "--env", type=str, help="Which environment to use", default="spread"
+        "-e",
+        "--env",
+        type=str,
+        help="Which environment to use",
+        default="spread",
     )
-    parser.add_argument("-s", "--steps", type=int, help="Steps to simulate", default=10)
+    parser.add_argument(
+        "-s",
+        "--steps",
+        type=int,
+        help="Steps to simulate",
+        default=10,
+    )
+    parser.add_argument(
+        "-r",
+        "--render",
+        type=str,
+        help="Render mode, default None",
+        default=None,
+    )
 
     args = parser.parse_args()
     if args.env == "spread":
@@ -67,7 +88,7 @@ if __name__ == "__main__":
         env_fn = knights_archers_zombies_v10
 
         env_kwargs = dict(
-            spawn_rate=20,
+            spawn_rate=6,
             num_archers=2,
             num_knights=2,
             max_zombies=10,
@@ -79,7 +100,7 @@ if __name__ == "__main__":
         print("Invalid env entered")
         exit(0)
 
-    env = env_fn.parallel_env(**env_kwargs)
+    env = env_fn.parallel_env(render_mode=args.render, **env_kwargs)
 
     try:
         latest_policy = max(
@@ -90,5 +111,8 @@ if __name__ == "__main__":
     except ValueError:
         print("Policy not found.")
         exit(0)
+
+    env = ss.pettingzoo_env_to_vec_env_v1(env)
+    env = ss.concat_vec_envs_v1(env, 1, num_cpus=1, base_class="stable_baselines3")
     pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(sim_steps(env, latest_policy))
+    pp.pprint(sim_steps(env, latest_policy, num_steps=args.steps))
